4a63b9d37cb9a46cc17584772006a699f4ead0d4
Andreas Monitzer <andreas@monitzer.com>
2017-09-04 04:05:25 +0200

    Added a red stop button to the top bar that stops whatever is active at the moment.

--- blinkenwall/app/controllers/application.js
+++ blinkenwall/app/controllers/application.js
@@ -1,12 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  serverConnection: Ember.inject.service(),
   actions: {
     showSidebar(sidebar) {
       Ember.$(`#${sidebar}`).sidebar('show');
     },
     hideSidebar(sidebar) {
       Ember.$(`#${sidebar}`).sidebar('hide');
-    }
+    },
+    stop() {
+      this.get('serverConnection').send({
+        cmd: "turnoff"
+      });
+    },
   }
 });
